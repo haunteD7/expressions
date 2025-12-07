6d3832656a5c80af22a05e639f3c5a9719f33953
haunteD7 <228pacan228aue@gmail.com>
2025-12-08 02:10:49 +0300

    add parser class

--- AST.h
+++ AST.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <memory>
+
+enum class Operation
+{
+  Add, 
+  Substract,
+  Negate,
+  Multipy,
+  Divide,
+  Power,
+};
+
 struct ASTNode
 {
-  
+  std::shared_ptr<ASTNode> left;
+  std::shared_ptr<ASTNode> right; 
+  Operation op;
 };
--- parser.h
+++ parser.h
@@ -1,2 +1,21 @@
 #pragma once
 
+#include "AST.h"
+#include "lexer.h"
+
+class Parser
+{
+public:
+  Parser(const std::vector<Token>& tokens) : tokens(tokens)
+  {
+
+  }
+  void parse() {
+    token_pos = 0;
+  }
+private:
+
+  const std::vector<Token>& tokens;
+  size_t token_pos = 0;
+  std::shared_ptr<ASTNode> ast;
+};
